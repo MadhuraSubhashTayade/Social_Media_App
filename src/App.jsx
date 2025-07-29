fb945e86714bd34e8f6dd7fbc6a94a8b1cbb7ed7
Madhura Tayade <tayademadhura02@gmail.com>
2025-07-29 18:56:25 +0530

    profile-route-added-with-outlet-and-page-section-containers-added-04

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,57 @@
 import "./App.css";
+import { LeftBar } from "./components/leftbar/LeftBar";
+import { NavBar } from "./components/navbar/NavBar";
+import { RightBar } from "./components/rightbar/RightBar";
 import { Login } from "./pages/login/Login";
 import { Register } from "./pages/register/Register";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Navigate,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+import { Home } from "./pages/home/Home";
+import { Profile } from "./pages/profile/Profile";
+
+const currentUser = true;
+const Layout = () => {
+  return (
+    <div>
+      <NavBar />
+      <div style={{ display: "flex" }}>
+        <LeftBar />
+        <Outlet />
+        <RightBar />
+      </div>
+    </div>
+  );
+};
+
+const ProtectedRoute = ({ children }) => {
+  if (!currentUser) {
+    return <Navigate to="/login" />;
+  }
+  return children;
+};
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <h1>Home</h1>,
+    element: (
+      <ProtectedRoute>
+        <Layout />
+      </ProtectedRoute>
+    ),
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/profile/:id",
+        element: <Profile />,
+      },
+    ],
   },
   {
     path: "/login",
---
+++ src/components/rightbar/RightBar.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const RightBar = () => {
+  return (
+    <div className='rightbar'>RightBar</div>
+  )
+}
