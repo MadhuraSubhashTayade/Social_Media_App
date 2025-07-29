4a897d30fefc36a44f1c61b59c5082550c99cdb7
Madhura Tayade <tayademadhura02@gmail.com>
2025-07-29 19:34:37 +0530

    website-navbar-added-05

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import "./navbar.scss";
+import { Link } from "react-router-dom";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import AppsOutlinedIcon from "@mui/icons-material/AppsOutlined";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import MailOutlineOutlinedIcon from "@mui/icons-material/MailOutlineOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 
 export const NavBar = () => {
   return (
-    <div className='navbar'>NavBar</div>
-  )
-}
+    <div className="navbar">
+      <div className="left">
+        <Link style={{ textDecoration: "none" }} to="/">
+          <span>CubSocial</span>
+        </Link>
+        <DarkModeOutlinedIcon />
+        <HomeOutlinedIcon />
+        <AppsOutlinedIcon />
+        <div className="search">
+          <SearchOutlinedIcon />
+          <input type="text" placeholder="Search here..." />
+        </div>
+      </div>
+      <div className="right">
+        <PersonOutlineOutlinedIcon />
+        <MailOutlineOutlinedIcon />
+        <NotificationsOutlinedIcon />
+        <div className="user">
+          <img
+            src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600"
+            alt="user-icon"
+          />
+          <span>John Doe</span>
+        </div>
+      </div>
+    </div>
+  );
+};
